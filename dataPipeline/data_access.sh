f3204fec29ecd1211021ed86261d18e87361a22f
Yoni Ackerman <jonathan.ackerman0@gmail.com>
2017-02-14 18:18:21 -0800

    made data access more efficient

--- dataPipeline/data_access.sh
+++ dataPipeline/data_access.sh
@@ -1,31 +1,22 @@
 #!/bin/bash 
 
-# example: bash data_access.sh variables.txt 2 models.txt 1
+# example: bash data_access.sh variables.txt 2 1
+tmpRes="ann"
+sptRes="g025"
+scenario="rcp26"
+ensemble="r1i1p1"
 
-# download the list of options for this variable
+head -n $2 $1 > tmp.txt
+
 RSYNC_PASSWORD=getdata rsync -vrlpt cmip5ng@atmos.ethz.ch::cmip5-ng |
-    grep -f $1 |
+    grep "$tmpRes" | grep "$sptRes" | grep "$scenario" |
+    grep "$ensemble" | grep -f tmp.txt |
     awk '{print $5}' > list.txt
 
-# loop through all the variables
+rm tmp.txt
+
 for var in $(head -n $2 $1); do
-    for mod in $(head -n $4 $3); do
-
-	# set the variables
-	tmpRes="mon"
-	sptRes="g025"
-	scenario="rcp26"
-	ensemble="r1i1p1"
-
-	# select the desired file names to tmp.txt
-	grep "$tmpRes" list.txt |
-	    grep ${var}_ |
-	    grep "$sptRes" |
-	    grep _${mod}_ |
-	    grep "$scenario" |
-	    grep "$ensemble" >> tmp.txt
-
-    done
+    grep ${var} list.txt | head -n $3 >> tmp.txt
 done
 
 # download desired files named in tmp.txt
