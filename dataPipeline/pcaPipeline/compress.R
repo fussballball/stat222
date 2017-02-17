3b60b8c083a781e7f76b4fecc7a6d758a03b9aec
Yoni Ackerman <jonathan.d.ackerman@gmail.com>
2017-02-17 13:38:38 -0800

    changed names, still working on pcaPipeline functionality

--- dataPipeline/pcaPipeline/compress.R
+++ dataPipeline/pcaPipeline/compress.R
@@ -14,16 +14,16 @@ varDat <- ldply(nc_files, function(nc){
     ncin <- nc_open(nc)
     lon <- ncvar_get(ncin, "lon")
     lat <- ncvar_get(ncin, "lat")
-    tas <- ncvar_get(ncin, "tas")
+    cVar <- ncvar_get(ncin, commandVar)
     ldply(1:length(lon), function(i){
         ldply(1:length(lat), function(j){
-            series <- tas[i,j,]
+            series <- cVar[i,j,]
             ddt_series <- series[-1] - series[-length(series)]
             data.frame(id = name,
                        variable = commandVar,
                        lon = lon[i],
                        lat = lat[i],
-                       var_dts = var(ddt_series))
+                       varDts = var(ddt_series))
         })
     })
 })
