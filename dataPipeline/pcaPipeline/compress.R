3dc13769368914d61fb7a38e6d2646ea8bb885cb
Yoni Ackerman <jonathan.d.ackerman@gmail.com>
2017-02-17 13:59:16 -0800

    pca data collection functions

--- dataPipeline/pcaPipeline/compress.R
+++ dataPipeline/pcaPipeline/compress.R
@@ -3,24 +3,28 @@
 ## get command line arguments
 args = commandArgs(trailingOnly=TRUE)
 
-nc_files <- list.files("~/Documents/Stat222/cmip5-ng/",
+nc_files <- list.files(paste0("~/Documents/Stat222/stat222/",
+                              "dataPipeline/pcaPipeline/cmip5-ng/"),
                        recursive = TRUE, full.names = TRUE)
 
 ## retrieve the variable passed by the commandline call
 commandVar <- args[1]
 
 varDat <- ldply(nc_files, function(nc){
-    name <- digest(nc)
+    model <- strsplit(nc, "_")[[1]][3]
     ncin <- nc_open(nc)
     lon <- ncvar_get(ncin, "lon")
     lat <- ncvar_get(ncin, "lat")
     cVar <- ncvar_get(ncin, commandVar)
+    ## loop through every lat/lon pair, compress the
+    ## time series at that point, and output a data.frame
+    ## of with the compressed data.
     ldply(1:length(lon), function(i){
         ldply(1:length(lat), function(j){
             series <- cVar[i,j,]
             ddt_series <- series[-1] - series[-length(series)]
-            data.frame(id = name,
-                       variable = commandVar,
+            data.frame(Model = model,
+                       Variable = commandVar,
                        lon = lon[i],
                        lat = lat[i],
                        varDts = var(ddt_series))
@@ -28,6 +32,7 @@ varDat <- ldply(nc_files, function(nc){
     })
 })
 
+## write the data to a csv file
 write.csv(varDat, file = paste0(commandVar,"_compressed.csv"))
 
 quit()
