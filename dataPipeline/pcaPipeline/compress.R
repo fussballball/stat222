a56ddd9ee86acbf4c458650b214ce92273ecdce8
Jonathan Ackerman <yoni@scf.Berkeley.EDU>
2017-03-03 15:04:02 -0800

    bug in compress.R

--- dataPipeline/pcaPipeline/compress.R
+++ dataPipeline/pcaPipeline/compress.R
@@ -5,7 +5,7 @@ source("../../code/yoni/functions.R")
 
 ## get command line arguments
 args = commandArgs(trailingOnly=TRUE)
-nc_files <- list.files("cmip5-ng/"), recursive = TRUE, full.names = TRUE)
+nc_files <- list.files("cmip5-ng/", recursive = TRUE, full.names = TRUE)
 
 ## retrieve the variable passed by the commandline call
 commandVar <- args[1]
@@ -16,7 +16,7 @@ M <- 30 ## compress spatially to M vars
 
 varDat <- ldply(nc_files, function(nc){
     model <- strsplit(nc, "_")[[1]][3]
-    cMat <- flatten_model(nc_open(nc))
+    cMat <- flatten_model(nc_open(nc), commandVar)
     ## perform two way pca:
     cMat <- tw_pca(cMat, N, M, TRUE, TRUE)
     ret <- data.frame(Model = model, as.list(as.vector(cMat)))
