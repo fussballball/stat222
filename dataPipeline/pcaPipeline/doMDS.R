b3724249aaf12b2dab417309b405c22498e9c7f1
YDAckerman <jonathan.ackerman0@gmail.com>
2017-03-20 22:22:33 -0700

    fixed ggplot bug

--- dataPipeline/pcaPipeline/doMDS.R
+++ dataPipeline/pcaPipeline/doMDS.R
@@ -8,7 +8,9 @@ fit <- cmdscale(d,eig=TRUE, k=2) # k is the number of dim
 x <- fit$points[,1]
 y <- fit$points[,2]
 plotDat <- data.frame(X = x, Y = y, names = pcaDat[,1])
-plt <-ggplot(plotDat, aes(x = X, y = Y, shape = names, color = names)) +
+
+plt <- ggplot(plotDat, aes(x = X, y = Y, shape = as.factor(names),
+                           color = as.factor(names))) +
     scale_shape_manual(values=seq(0,20)) + 
     geom_point()
 
